785c27a4e314d08bf3a8fd3198855f90314ac08c
Qingtian Wang <wqt.net@gmail.com>
2022-05-08 15:50:50 -0500

    patch: better synchronization on stitcher

--- src/main/java/chunk4j/ChunkStitcher.java
+++ src/main/java/chunk4j/ChunkStitcher.java
@@ -23,19 +23,11 @@ package chunk4j;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.RemovalCause;
-import static com.github.benmanes.caffeine.cache.RemovalCause.EXPIRED;
-import static com.github.benmanes.caffeine.cache.RemovalCause.SIZE;
 import com.github.benmanes.caffeine.cache.RemovalListener;
-import com.google.common.collect.Interner;
-import com.google.common.collect.Interners;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
+import lombok.Data;
+
+import javax.annotation.Nonnull;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -48,13 +40,25 @@ public final class ChunkStitcher implements Stitcher {
     private static final Logger LOG = Logger.getLogger(ChunkStitcher.class.getName());
     private static final long DEFAULT_MAX_CHUNK_GROUP_COUNT = Long.MAX_VALUE;
     private static final long DEFAULT_MAX_STITCH_TIME_MILLIS = Long.MAX_VALUE;
+    private final Cache<UUID, Set<Chunk>> chunkGroups;
+
+    private ChunkStitcher(Builder builder) {
+        final long maxStitchTimeMillis =
+                builder.maxStitchTimeMillis == null ? DEFAULT_MAX_STITCH_TIME_MILLIS : builder.maxStitchTimeMillis;
+        final long maxGroups = builder.maxGroups == null ? DEFAULT_MAX_CHUNK_GROUP_COUNT : builder.maxGroups;
+        this.chunkGroups = Caffeine.newBuilder()
+                .expireAfterWrite(maxStitchTimeMillis, TimeUnit.MILLISECONDS)
+                .maximumSize(maxGroups)
+                .evictionListener(new InvoluntaryEvictionLogger(maxStitchTimeMillis, maxGroups))
+                .build();
+    }
 
     private static byte[] stitchAll(Set<Chunk> group) {
         LOG.log(Level.FINER, () -> "Start stitching all chunks in group: " + group);
         assert ofSameId(group);
         byte[] groupBytes = new byte[getTotalByteSize(group)];
         List<Chunk> orderedGroup = new ArrayList<>(group);
-        Collections.sort(orderedGroup, (chunk1, chunk2) -> chunk1.getIndex() - chunk2.getIndex());
+        orderedGroup.sort(Comparator.comparingInt(Chunk::getIndex));
         int groupBytesPosition = 0;
         for (Chunk chunk : orderedGroup) {
             final int chunkBytesLength = chunk.getBytes().length;
@@ -66,55 +70,41 @@ public final class ChunkStitcher implements Stitcher {
     }
 
     private static int getTotalByteSize(Set<Chunk> group) {
-        return group.stream()
-                .mapToInt(chunk -> chunk.getBytes().length)
-                .sum();
+        return group.stream().mapToInt(chunk -> chunk.getBytes().length).sum();
     }
 
     private static boolean ofSameId(Set<Chunk> group) {
-        return group.stream()
-                .map(chunk -> chunk.getGroupId())
-                .distinct()
-                .count() == 1;
+        return group.stream().map(Chunk::getGroupId).distinct().count() == 1;
     }
 
-    private final Cache<UUID, Set<Chunk>> chunkGroups;
-    private final Interner interner;
-
-    private ChunkStitcher(Builder builder) {
-        final long maxStitchTimeMillis = builder.maxStitchTimeMillis == null ? DEFAULT_MAX_STITCH_TIME_MILLIS
-                : builder.maxStitchTimeMillis;
-        final long maxGroups = builder.maxGroups == null ? DEFAULT_MAX_CHUNK_GROUP_COUNT : builder.maxGroups;
-        this.chunkGroups = Caffeine.newBuilder()
-                .expireAfterWrite(maxStitchTimeMillis, TimeUnit.MILLISECONDS)
-                .maximumSize(maxGroups)
-                .evictionListener(new InvoluntaryEvictionLogger(maxStitchTimeMillis, maxGroups))
-                .build();
-        this.interner = Interners.newWeakInterner();
-    }
-
-    @Override
-    public Optional<byte[]> stitch(Chunk chunk) {
+    @Override public Optional<byte[]> stitch(Chunk chunk) {
         LOG.log(Level.FINER, () -> "Received chunk: " + chunk);
         final UUID groupId = chunk.getGroupId();
-        synchronized (this.interner.intern(groupId)) {
-            final Set<Chunk> chunks = chunkGroups.get(groupId, key -> new HashSet<>());
-            if (!chunks.add(chunk)) {
-                LOG.log(Level.WARNING, "Ignoring duplicate chunk: {0}", chunk);
-                return Optional.empty();
+        CompleteGroupHolder completeGroupHolder = new CompleteGroupHolder();
+        chunkGroups.asMap().compute(groupId, (gid, group) -> {
+            if (group == null) {
+                group = new HashSet<>();
             }
-            final int receivedTotal = chunks.size();
-            final int expectedTotal = chunk.getGroupSize();
+            if (!group.add(chunk)) {
+                LOG.log(Level.WARNING, "received duplicate chunk: {0}", chunk);
+            }
+            int receivedTotal = group.size();
+            int expectedTotal = chunk.getGroupSize();
             if (receivedTotal != expectedTotal) {
-                LOG.log(Level.FINEST, () -> "Received: " + receivedTotal + " chunks, awaiting expected: "
-                        + expectedTotal + " chunks to start restoring");
-                return Optional.empty();
+                LOG.log(Level.FINEST,
+                        () -> "Received: " + receivedTotal + " chunks, awaiting expected: " + expectedTotal
+                                + " chunks to start restoring");
+                return group;
             }
-            LOG.log(Level.FINE, () -> "Received all: " + expectedTotal
-                    + " expected chunks, starting to restore orginal data...");
-            chunkGroups.invalidate(groupId);
-            return Optional.of(stitchAll(chunks));
+            LOG.log(Level.FINE, () -> "Received all: " + expectedTotal + " expected chunks");
+            completeGroupHolder.setCompleteGroupOfChunks(group);
+            return null;
+        });
+        Set<Chunk> groupChunks = completeGroupHolder.getCompleteGroupOfChunks();
+        if (groupChunks == null) {
+            return Optional.empty();
         }
+        return Optional.of(stitchAll(groupChunks));
     }
 
     public static class Builder {
@@ -148,26 +138,21 @@ public final class ChunkStitcher implements Stitcher {
             this.maxGroups = maxGroups;
         }
 
-        @Override
-        public void onRemoval(UUID groupId, Set<Chunk> chunks, RemovalCause cause) {
-            Objects.requireNonNull(cause);
+        @Override public void onRemoval(UUID groupId, Set<Chunk> chunks, @Nonnull RemovalCause cause) {
             switch (cause) {
                 case EXPIRED:
                     LOG.log(Level.SEVERE,
                             "Chunk group {0} took too long to stitch and expired after {1} milliseconds, expected {2} chunks but only received {3} when expired",
-                            new Object[] { groupId, maxStitchTimeMillis, chunks.stream()
-                                    .findFirst()
-                                    .get()
-                                    .getGroupSize(), chunks.size() });
+                            new Object[] { groupId, maxStitchTimeMillis,
+                                    chunks.stream().findFirst().orElseThrow(NoSuchElementException::new).getGroupSize(),
+                                    chunks.size() });
                     break;
                 case SIZE:
                     LOG.log(Level.SEVERE, "Chunk group {0} was removed due to exceeding max group count {1}",
                             new Object[] { groupId, maxGroups });
                     break;
                 case EXPLICIT:
-                    break;
                 case REPLACED:
-                    break;
                 case COLLECTED:
                     break;
                 default:
@@ -176,4 +161,8 @@ public final class ChunkStitcher implements Stitcher {
         }
     }
 
+    @Data private static class CompleteGroupHolder {
+
+        Set<Chunk> completeGroupOfChunks;
+    }
 }
--- src/test/java/chunk4j/ByteChunksTest.java
+++ src/test/java/chunk4j/ByteChunksTest.java
@@ -20,11 +20,13 @@
 
 package chunk4j;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Qingtian Wang
@@ -35,8 +37,7 @@ public class ByteChunksTest {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     private static final String DATA_TEXT2 = DATA_TEXT1 + DATA_TEXT1;
 
-    @Test
-    public void testByteChunks() {
+    @Test public void testByteChunks() {
         final int chunkByteCapacity = 4;
         final ChunkChopper chopper = ChunkChopper.ofChunkByteSize(chunkByteCapacity);
         final ChunkStitcher stitcher = new ChunkStitcher.Builder().build();
@@ -47,8 +48,7 @@ public class ByteChunksTest {
         Collections.shuffle(choppedMingledAndScrambledData);
         final List<byte[]> stitched = new ArrayList<>();
         choppedMingledAndScrambledData.forEach(chunk -> {
-            stitcher.stitch(chunk)
-                    .ifPresent(originalDataBytes -> stitched.add(originalDataBytes));
+            stitcher.stitch(chunk).ifPresent(stitched::add);
         });
 
         final int originalDataUnits = 2;
